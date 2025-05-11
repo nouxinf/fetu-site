6d690056faf15c8a79ec3b87bbf5d43a7f44aaee
nouxinf <bingchilling1915@gmail.com>
2025-05-11 16:15:14 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/fetu/",
+  base: "https://nouxinf.github.io/fetu/",
 })
