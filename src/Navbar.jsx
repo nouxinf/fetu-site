9cb1cacc80eff8f118a63db8407172798a6845b3
nouxinf <bingchilling1915@gmail.com>
2025-05-11 10:03:54 +0100

    public fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import { useTranslation } from 'react-i18next';
 const Navbar = () => {
+    const { t } = useTranslation();
     return (
         <nav className="navbar">
             <div className="navbar-logo">
                 <a href="/">Logo</a>
             </div>
             <ul className="navbar-links">
-                <li><a href="#home">Home</a></li>
+                <li><a href="#home">{t("navbar.home")}</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#services">Map</a></li>
             </ul>
         </nav>
     );
