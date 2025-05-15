2efe485163e2e6c755155e3a28abb5ddd234ff1c
nouxinf <bingchilling1915@gmail.com>
2025-05-15 20:41:51 +0100

    translation for banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ReactSVG } from 'react-svg';
-import i18n from '/src/i18n/index.jsx';
-import bannerImg from "/src/assets/fetu-banner.svg"
+import { useTranslation } from 'react-i18next';
+import bannerImgEn from "/src/assets/fetu-banner-en.svg"
 
 const Banner = () => {
+    const { t } = useTranslation();
+
+    const images = {
+        en: bannerImgEn
+    };
+    const bannerSrc = images[t.language] || bannerImgEn
     return (
         <>
         <ReactSVG
-         src={bannerImg}/>
+         src={bannerSrc}/>
         </>
     );
 };
