15731e1a136083bc49b4c829b41ac70e7d8b57fa
nouxinf <bingchilling1915@gmail.com>
2025-05-15 20:37:08 +0100

    more fixes for horizonal screens

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { ReactSVG } from 'react-svg'
+import { ReactSVG } from 'react-svg';
+import i18n from '/src/i18n/index.jsx';
 import bannerImg from "/src/assets/fetu-banner.svg"
+
 const Banner = () => {
     return (
         <>
